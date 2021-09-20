6484c9ac745f80c3755436088992f888e91df451
Михаил <kriptonit1988@gmail.com>
2021-09-20 13:11:31 +0500

    Реализовал создание устройства

--- server/controllers/device-controller.js
+++ server/controllers/device-controller.js
@@ -1,6 +1,34 @@
-class DeviceController {
-  async create(request, response) {
+const path = require("path");
 
+const { v4 } = require("uuid");
+
+const ApiError = require("../error/api-error");
+
+const { Device } = require("../models");
+
+class DeviceController {
+  async create(request, response, next) {
+    try {
+      const { name, price, brandId, typeId, info } = request.body;
+      const { img } = request.files;
+      
+      const filename = `${v4()}.jpg`;
+      const imgPath = path.resolve(__dirname, "..", "static", filename);
+
+      img.mv(imgPath);
+
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        img: filename
+      });
+
+      return response.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
   }
 
   async getAll(request, response) {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
+const path = require("path");
+
 const express = require("express");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 const db = require("./db");
 // const models = require("./models");
@@ -12,6 +15,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
 
 // Обработка ошибок - обязательно самый последний из возможных middlewares
