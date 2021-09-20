60c6657478b2e85c559f7bc77514356168388d94
Михаил <kriptonit1988@gmail.com>
2021-09-20 11:35:30 +0500

    Реализовал создание типа

--- server/controllers/type-controller.js
+++ server/controllers/type-controller.js
@@ -1,6 +1,13 @@
+const { Type } = require("../models");
+const ApiError = require("../error/api-error");
+
 class TypeController {
   async create(request, response) {
+    const { name } = request.body;
 
+    const type = await Type.create({ name });
+
+    return response.json(type);
   }
 
   async getAll(request, response) {
