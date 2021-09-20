881420ee249ba204707a72b3f956bad0fc721413
Михаил <kriptonit1988@gmail.com>
2021-09-20 14:45:21 +0500

    Реализовал контроллер для регистрации нового пользователя

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,38 @@
+const bcrypt = require("bcrypt");
+
 const ApiError = require("../error/api-error");
+const { Basket, User } = require("../models");
+const { generateJwt } = require("../utils/user");
 
 class UserController {
-  async registration(request, response) {
+  async registration(request, response, next) {
+    const { email, password, role } = request.body;
 
+    if (!email || !password) return next(ApiError.badRequest("Отсутствует email или пароль!"));
+
+    const candidate = await User.findOne({
+      where: {
+        email
+      }
+    });
+
+    // Если уже существует пользователь с таким email
+    if (candidate) return next(ApiError.badRequest("Пользователь с таким email уже зарегистрирован!"));
+
+    // Второй параметр - сколько раз хэшировать. Слишком много не нужно
+    const hashPassword = await bcrypt.hash(password, 5);
+
+    const user = await User.create({
+      email,
+      role,
+      password: hashPassword
+    });
+
+    const basket = Basket.create({ userId: user.id });
+
+    const token = generateJwt(user);
+
+    return response.json({ token });
   }
 
   async login(request, response) {
---
+++ server/utils/user.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const generateJwt = ({ id, email, role }) => {
+  return jwt.sign({
+    id,
+    email,
+    role
+  },
+  process.env.JWT_SECRET_KEY,
+  {
+    expiresIn: "24h"
+  });
+};
+
+module.exports = {
+  generateJwt
+};
